d9d11b05e5d22137f83b987e19c35d1c98d366f4
Johyechan <johyechan6@gmail.com>
2025-02-26 17:07:45 +0900

    2 Day

---
+++ Study/Study/대소문자 바꿔서 출력하기.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	string str;
+//	cin >> str;
+//	for (int i = 0; i < str.size(); i++)
+//	{
+//		if (islower(str[i]))
+//		{
+//			cout << static_cast<char>(toupper(str[i]));
+//		}
+//		else
+//		{
+//			cout << static_cast<char>(tolower(str[i]));
+//		}
+//	}
+//}
